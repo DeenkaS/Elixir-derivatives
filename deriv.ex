c10fc16784c104a755592467aa41783c801e7e61
Deniel Saskin <Deniel.saskin@gmail.com>
2023-01-19 11:39:59 +0100

    fixed some more Derivatives to include chain rule

--- deriv.ex
+++ deriv.ex
@@ -9,20 +9,18 @@ defmodule Deriv do
  def deriv({:var,_},_) do {:num,0} end
  def deriv({:add,e1,e2},x) do {:add,deriv(e1,x),deriv(e2,x)} end
  def deriv({:mul,e1,e2},x) do {:add,{:mul,deriv(e1,x),e2},{:mul,e1,deriv(e2,x)}} end
- def deriv({:ln,x},x) do {:mul,{:div,1,x},deriv(x)}  end
+ def deriv({:ln,x},x) do {:mul,{:div,1,x},deriv(x,x)}  end
  def deriv({:div,a,x},x) do {:div,{:num,-a},{:pow,x,2}} end
  def deriv({:pow,x,a},x) do {:mul,a,{:pow,x,{:num, a-1}}} end
  def deriv({:Sqrt,x},x) do {:mul,{:div,1,2},{:div,1,{:sqrt,x}}} end
- def deriv({:trig,:sin,x},x) do {:trig,:cos,x} end
- def deriv({:trig,:cos,x},x) do {:mul,{:trig,:sin,x},{:num,-1}} end
-
+ def deriv({:trig,:sin,x},x) do {:mul,deriv(x,x){:trig,:cos,x}} end
+ def deriv({:trig,:cos,x},x) do {:mul,deriv(x,x),{:mul,{:trig,:sin,x},{:num,-1}}} end
 
  def simplify({:num,a}) do a end
  def simplify({:var,x}) do x end
  def simplify({:add,e1,e2}) do simplify_add(simplify(e1),simplify(e2)) end
  def simplify({:mul,e1,e2}) do simplify_mul(simplify(e1),simplify(e2)) end
  def simplify({:div,e1,e2}) do simplify_div(simplify(e1),simplify(e2)) end
-
 
  def simplify_add(e1,{:num,0}) do e1 end
  def simplify_add({:num,0},e1) do e1 end
@@ -35,8 +33,6 @@ defmodule Deriv do
  def simplify_mul(e1,e2) do {:mul,e1,e2} end
  def simplify_mul(e1,{:num,1}) do e1 end
  def simplify_mul({:num,1},e1) do e1 end
-
-
 
  def pprint({:num, n}) do "#{n}" end
  def pprint({:var, v}) do "#{v}" end
